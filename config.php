ed71e8de693d0bee56f703c4340c7c5929a31e21
dassuptirani <55455884+dassuptirani@users.noreply.github.com>
2019-12-12 10:48:20 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$host_name ='localhost';
+$name ='root';
+$pass ='';
+$db ='test';
+
+$con=mysqli_connect($host_name,$name,$pass) or die('Database Error !');
+mysqli_select_db($con,$db);
+
+?>
